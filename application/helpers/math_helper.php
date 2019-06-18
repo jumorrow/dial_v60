ed4e329fd18c1be3d0375f07d531a22ae4e1969e
TravDev <traverslaville@gmail.com>
2019-06-18 12:34:44 -0500

    added nearest_tenth function to math helper

--- application/helpers/math_helper.php
+++ application/helpers/math_helper.php
@@ -7,6 +7,27 @@
  *
  * @package	CodeIgniter
  * @author	Justin Morrow <jumorrow@protonmail.com>
+ * @author Travers La Ville
  */
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+if (!function_exists('nearest_tenth'))
+{
+    /**
+     * Round number to nearest tenth
+     *
+     * @return	int
+     */    
+    function nearest_tenth($ugly_number)
+    {
+        //determine smaller multiple
+        $smaller_number = (int)($ugly_number / 10) * 10;  
+
+        //determine larger multiple  
+        $larger_number = ($smaller_number + 10);  
+
+        //return the value closest to the two
+        return ($ugly_number - $smaller_number > $larger_number - $ugly_number) ? $larger_number : $smaller_number;  
+    }   
+    
+}
