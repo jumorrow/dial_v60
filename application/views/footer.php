29d2912b967fa3e75166ae6945bb06ce5c00b15f
TravDev <traverslaville@gmail.com>
2019-06-19 17:50:39 -0500

    removed social media icons and added reset button to footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,15 +1,17 @@
 <section id="footer">
-        <ul class="icons">
-            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-            <li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
-            <li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
-            <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-            <li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-        </ul>
-        <div class="copyright">
-            <ul class="menu">
-                <li>&copy; <?php echo get_current_year(); ?> Dial v60. All rights reserved.</li>
+ <!-- Reset Button -->       
+<button class="icons" type ="reset" value = "Reset" onClick="window.location.reload()"> Reset </button>
+<!--<ul class="icons">-->
+<!--<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>-->
+<!--</ul>-->
+    <div class="copyright">
+        <ul class="menu">
+            <li>&copy; <?php echo get_current_year(); ?> Dial v60. All rights reserved.</li>
             </ul>
-        </div>
-    </section>
+    </div>
+</section>
